0525d0c8960edfa370c463291a13ff7c5fd4b2f2
arwa <arwam.rabah@gmail.com>
2022-07-25 00:38:38 +0300

    This is my new HOME !

---
+++ my_first_project/App1/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+from django.http import HttpRequest,HttpResponse
+# Create your views here.
+
+
+
+def home(request : HttpRequest):
+    
+    phrase: str="Hello World, This is my new HOME !"
+
+    return HttpResponse(phrase)
