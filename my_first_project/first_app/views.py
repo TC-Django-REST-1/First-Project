322ae2366c459f040b2dd00838a273631b1fd6f3
Nawaf Sh <nawaf.sheddi@gmail.com>
2022-07-24 21:31:47 +0300

    lab 1

---
+++ my_first_project/first_app/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+from django.http import HttpRequest, HttpResponse
+# Create your views here.
+
+
+def hello(request : HttpRequest):
+    res :str = 'Hello World, This is my new HOME !'
+
+    return HttpResponse(res)
