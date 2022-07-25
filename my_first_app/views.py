b45bd242062f5f50db424c63191041a30218f3d7
saraalrumih <saraalrumih@gmail.com>
2022-07-25 15:27:14 +0300

    1st lab submission

---
+++ my_first_app/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+from django.http import HttpRequest, HttpResponse
+
+# Create your views here.
+
+
+def hello(request : HttpRequest):
+    
+    phrase : str = "Hello World, I love Django!"
+    
+    return HttpResponse(phrase)
