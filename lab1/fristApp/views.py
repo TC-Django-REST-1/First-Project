9a410d21e573d16d94859b08f2dc86b6dbcc5b0a
rafif <rafeefalazzaz@gmail.com>
2022-07-25 18:21:10 +0300

    solve frist lab

--- lab1/fristApp/views.py
+++ lab1/fristApp/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.http import HttpRequest, HttpResponse , HttpResponse 
+from django.http import HttpRequest, HttpResponse  
 # Create your views here.
 def hello(request : HttpRequest):
     msg='Hello World, This is my new HOME !'
